233dc848ca9a290cdad0007714f96eac79696cce
Michael Roscoe <mikeroscoe18@gmail.com>
2019-11-25 20:57:28 -0400

    Changed some of the output and the logic of the function calls.

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -21,11 +21,11 @@ public class Main {
     }
 
     public static void main(String[] args) throws PcapNativeException, NotOpenException {
-        // The code we had before
+
+        // Get the network device
         PcapNetworkInterface device = getNetworkDevice();
         System.out.println("You chose: " + device);
 
-        // New code below here
         if (device == null) {
             System.out.println("No device chosen.");
             System.exit(1);
@@ -42,61 +42,59 @@ public class Main {
         //handle.setFilter(filter, BpfCompileMode.OPTIMIZE);
 
         // Create a listener that defines what to do with the received packets
-        PacketListener listener = new PacketListener() {
-            @Override
-            public void gotPacket(Packet packet) {
-                // Print packet information to screen
-                System.out.println("\n------------Packet Info:---------------");
+        PacketListener listener = packet -> {
+            // Print packet information to screen
+            System.out.println("\n------------Packet Info:---------------");
 
-                //Handle the Ethernet portion of the packet
-                EthernetPacket ethernetPacket = packet.get(EthernetPacket.class);
-                if (ethernetPacket != null) {
-                    EthernetPacket.EthernetHeader ethernetHeader = ethernetPacket.getHeader();
-                    System.out.println("Protocol: " + ethernetHeader.getType());
-                }
-
-                //Handle the IP Portion of the information
-                IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
-                IpV6Packet ipV6Packet = packet.get(IpV6Packet.class);
-                if (ipV4Packet != null) {
-                    IpV4Packet.IpV4Header header = ipV4Packet.getHeader();
-                    System.out.println("Source IP: " + header.getSrcAddr());
-                    System.out.println("Destination IP: " + header.getDstAddr());
-                }
-                if (ipV6Packet != null) {
-                    IpV6Packet.IpV6Header header = ipV6Packet.getHeader();
-                    System.out.println("Source IP: " + header.getSrcAddr());
-                    System.out.println("Destination IP: " + header.getDstAddr());
-                }
-
-                //Handle the TCP portion of the packet
-                TcpPacket tcpPacket = packet.get(TcpPacket.class);
-                if (tcpPacket != null) {
-                    TcpPacket.TcpHeader header = tcpPacket.getHeader();
-                    System.out.println("Source Port: " + header.getSrcPort());
-                    System.out.println("Destination Port: " + header.getDstPort());
-                }
-
-                //Handle the UDP portion of the packet
-                UdpPacket udpPacket = packet.get(UdpPacket.class);
-                if (udpPacket != null) {
-                    UdpPacket.UdpHeader header = udpPacket.getHeader();
-                    System.out.println("Source Port: " + header.getSrcPort());
-                    System.out.println("Destination Port: " + header.getDstPort());
-                }
-
+            //Handle the Ethernet portion of the packet
+            EthernetPacket ethernetPacket = packet.get(EthernetPacket.class);
+            if (ethernetPacket != null) {
+                EthernetPacket.EthernetHeader ethernetHeader = ethernetPacket.getHeader();
+                System.out.println("Protocol: " + ethernetHeader.getType());
             }
+
+            //Handle the IP Portion of the information
+            IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
+            IpV6Packet ipV6Packet = packet.get(IpV6Packet.class);
+            if (ipV4Packet != null) {
+                IpV4Packet.IpV4Header header = ipV4Packet.getHeader();
+                System.out.println("Source IP: " + header.getSrcAddr());
+                System.out.println("Destination IP: " + header.getDstAddr());
+            }
+            if (ipV6Packet != null) {
+                IpV6Packet.IpV6Header header = ipV6Packet.getHeader();
+                System.out.println("Source IP: " + header.getSrcAddr());
+                System.out.println("Destination IP: " + header.getDstAddr());
+            }
+
+            //Handle the TCP portion of the packet
+            TcpPacket tcpPacket = packet.get(TcpPacket.class);
+            if (tcpPacket != null) {
+                TcpPacket.TcpHeader header = tcpPacket.getHeader();
+                System.out.println("Source Port: " + header.getSrcPort());
+                System.out.println("Destination Port: " + header.getDstPort());
+            }
+
+            //Handle the UDP portion of the packet
+            UdpPacket udpPacket = packet.get(UdpPacket.class);
+            if (udpPacket != null) {
+                UdpPacket.UdpHeader header = udpPacket.getHeader();
+                System.out.println("Source Port: " + header.getSrcPort());
+                System.out.println("Destination Port: " + header.getDstPort());
+            }
+
         };
 
         // Tell the handle to loop using the listener we created
         try {
-            int maxPackets = 50;
-            handle.loop(maxPackets, listener);
+            //TODO: Make infinite for final submission.
+            while (true) {
+                handle.loop(10, listener);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Print out handle statistics
         PcapStat stats = handle.getStats();
         System.out.println("\n------------Final Stats for packet capture:------------");
         System.out.println("Packets received: " + stats.getNumPacketsReceived());
