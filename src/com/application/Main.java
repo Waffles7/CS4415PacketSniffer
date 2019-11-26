64b294a996e3909771e5b6426b364cbf0aa44ad4
Michael Roscoe <mikeroscoe18@gmail.com>
2019-11-25 20:24:41 -0400

    Modified the packet output to be what we are actually interested in.

--- src/com/application/Main.java
+++ src/com/application/Main.java
@@ -1,10 +1,9 @@
 package com.application;
 
 import com.sun.jna.Platform;
-import org.pcap4j.core.BpfProgram.BpfCompileMode;
 import org.pcap4j.core.*;
 import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
-import org.pcap4j.packet.Packet;
+import org.pcap4j.packet.*;
 import org.pcap4j.util.NifSelector;
 
 import java.io.IOException;
@@ -37,26 +36,55 @@ public class Main {
         int readTimeout = 50; // in milliseconds
         final PcapHandle handle;
         handle = device.openLive(snapshotLength, PromiscuousMode.PROMISCUOUS, readTimeout);
-        PcapDumper dumper = handle.dumpOpen("out.pcap");
 
         // Set a filter to only listen for tcp packets on port 80 (HTTP)
-//        String filter = "tcp port 80";
-//        handle.setFilter(filter, BpfCompileMode.OPTIMIZE);
+        //String filter = "tcp port 80";
+        //handle.setFilter(filter, BpfCompileMode.OPTIMIZE);
 
         // Create a listener that defines what to do with the received packets
         PacketListener listener = new PacketListener() {
             @Override
             public void gotPacket(Packet packet) {
                 // Print packet information to screen
-                System.out.println(handle.getTimestamp());
-                System.out.println(packet);
+                System.out.println("\n------------Packet Info:---------------");
 
-                // Dump packets to file
-                try {
-                    dumper.dump(packet, handle.getTimestamp());
-                } catch (NotOpenException e) {
-                    e.printStackTrace();
+                //Handle the Ethernet portion of the packet
+                EthernetPacket ethernetPacket = packet.get(EthernetPacket.class);
+                if (ethernetPacket != null) {
+                    EthernetPacket.EthernetHeader ethernetHeader = ethernetPacket.getHeader();
+                    System.out.println("Protocol: " + ethernetHeader.getType());
                 }
+
+                //Handle the IP Portion of the information
+                IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
+                IpV6Packet ipV6Packet = packet.get(IpV6Packet.class);
+                if (ipV4Packet != null) {
+                    IpV4Packet.IpV4Header header = ipV4Packet.getHeader();
+                    System.out.println("Source IP: " + header.getSrcAddr());
+                    System.out.println("Destination IP: " + header.getDstAddr());
+                }
+                if (ipV6Packet != null) {
+                    IpV6Packet.IpV6Header header = ipV6Packet.getHeader();
+                    System.out.println("Source IP: " + header.getSrcAddr());
+                    System.out.println("Destination IP: " + header.getDstAddr());
+                }
+
+                //Handle the TCP portion of the packet
+                TcpPacket tcpPacket = packet.get(TcpPacket.class);
+                if (tcpPacket != null) {
+                    TcpPacket.TcpHeader header = tcpPacket.getHeader();
+                    System.out.println("Source Port: " + header.getSrcPort());
+                    System.out.println("Destination Port: " + header.getDstPort());
+                }
+
+                //Handle the UDP portion of the packet
+                UdpPacket udpPacket = packet.get(UdpPacket.class);
+                if (udpPacket != null) {
+                    UdpPacket.UdpHeader header = udpPacket.getHeader();
+                    System.out.println("Source Port: " + header.getSrcPort());
+                    System.out.println("Destination Port: " + header.getDstPort());
+                }
+
             }
         };
 
@@ -70,6 +98,7 @@ public class Main {
 
         // Print out handle statistics
         PcapStat stats = handle.getStats();
+        System.out.println("\n------------Final Stats for packet capture:------------");
         System.out.println("Packets received: " + stats.getNumPacketsReceived());
         System.out.println("Packets dropped: " + stats.getNumPacketsDropped());
         System.out.println("Packets dropped by interface: " + stats.getNumPacketsDroppedByIf());
@@ -79,7 +108,6 @@ public class Main {
         }
 
         // Cleanup when complete
-        dumper.close();
         handle.close();
     }
 }
